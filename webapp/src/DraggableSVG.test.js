cd63ab9a0375ad0916c026c504ec8d6337ecb874
W. Trevor King <wking@tremily.us>
2016-11-23 02:25:16 -0800

    DraggableSVG: Test event handlers
    This gives us complete test coverage for DraggableSVG according to:
    
      $ npm test -- --coverage

--- webapp/src/DraggableSVG.test.js
+++ webapp/src/DraggableSVG.test.js
@@ -14,3 +14,31 @@ it('renders without crashing', () => {
     div
   );
 });
+
+it('drag and drop shifts viewBox', () => {
+  const div = document.createElement('div');
+  return new Promise(
+    function (resolve, reject) {
+      ReactDOM.render(
+        <DraggableSVG width={800} height={600} scale={10}>
+          <rect x={40} y={30} width={6} height={2}
+            style={{fillOpacity: '0.1'}}>
+          </rect>
+        </DraggableSVG>,
+        div,
+        function () {
+          this.handleMouseMove({clientX: 30, clientY: 50});
+          expect(this.state.dx).toBe(0);
+          expect(this.state.dy).toBe(0);
+          this.startDrag({clientX: 10, clientY: 20});
+          this.handleMouseMove({clientX: 30, clientY: 50});
+          expect(this.state.dx).toBe(-2);
+          expect(this.state.dy).toBe(-3);
+          this.stopDrag();
+          expect(this.state.start).toBe(null);
+          resolve();
+        },
+      );
+    },
+  );
+});
